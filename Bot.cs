4d86b7731d50ff6b385d78bb7b007102552ee322
Nequs <Nequs@DESKTOP-KD93DQM>
2022-03-10 21:08:07 +0100

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,55 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Windows;
+using HostCord.Services;
+
+namespace HostCord
+{
+    internal class Bot
+    {
+        public Bot()
+        {
+
+        }
+
+        public async Task MainAsync(string token)
+        {
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                await client.LoginAsync(TokenType.Bot, token);
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Critical
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    LogLevel = LogSeverity.Critical
+                }))
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ Services/CommandHandlingService.cs
@@ -0,0 +1,58 @@
+﻿
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HostCord.Services
+{
+    public class CommandHandlingService
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _discord;
+        private readonly IServiceProvider _services;
+
+        public CommandHandlingService(IServiceProvider services, DiscordSocketClient client)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _discord = client;
+            _services = services;
+
+            _commands.CommandExecuted += CommandExecutedAsync;
+            _discord.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task MessageReceivedAsync(SocketMessage rawMessage)
+        {
+            if (!(rawMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            var prefPos = 0;
+            if (!message.HasCharPrefix(char.Parse("+"), ref prefPos)) return;
+
+            var context = new SocketCommandContext(_discord, message);
+
+            await _commands.ExecuteAsync(context, prefPos, _services);
+        }
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            if (context.Message.Author.IsBot)
+                return;
+
+            if (!command.IsSpecified)
+                return;
+
+            if (result.IsSuccess)
+                return;
+        }
+    }
+}
